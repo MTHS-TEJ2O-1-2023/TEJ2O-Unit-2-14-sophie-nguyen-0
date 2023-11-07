edb3fc599f1f2ea338d76ffd0e3d95676eb20bf1
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-07 12:35:20 -0500

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: sophie
+ * Created on: oct 2023
+ * This program makes LED sprite move diagonally
 */
 
-basic.showString('Hello, World!')
+// variable
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.showIcon(IconNames.Heart)
+basic.pause(500)
+basic.clearScreen()
+
+// p seress "A" button
+input.onButtonPressed(Button.A, function () {
+  // setup
+  basic.clearScreen()
+  loopCounter = 0
+  sprite = game.createSprite(0, 0)
+  while (loopCounter <= 5) {
+    basic.pause(500)
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    loopCounter ++
+  }
+  sprite.delete()
+  basic.showIcon(IconNames.Rabbit)
+})
+
+// press "B" button
+input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
+  loopCounter = 5
+  sprite = game.createSprite(5, 5)
+  while (loopCounter >= -1) {
+    basic.pause(500)
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    loopCounter --
+  }
+  sprite.delete()
+  basic.showIcon(IconNames.Rabbit)
+})
