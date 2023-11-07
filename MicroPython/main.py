b6d4a660580eb3b32cab233bbf92645ca49ff77f
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-07 18:26:20 +0000

    py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,50 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Sophie
+Created on: Oct 2023
+This module is a Micro:bit MicroPython program that makes LED sprite move diagonally
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# variables
+loop_counter = 0
+
+# setup
+display.clear()
+display.show(Image.HEART)
+sleep(1000)
+
+# loop
+while True:
+    if button_a.is_pressed():
+        # setup
+        display.clear()
+        loop_counter = 0
+
+        # loop
+        while loop_counter <= 5:
+            sleep(500)
+
+            set_pixel(loop_counter, loop_counter, 9)
+
+            # loop_counter goes up by 1
+            loop_counter += 1
+
+        display.show(Image.HEART)
+
+    if button_b.is_pressed():
+        # setup
+        display.clear()
+        loop_counter = 5
+
+        # loop
+        while loop_counter <= 5:
+            sleep(500)
+
+            set_pixel(loop_counter, loop_counter, 9)
+
+            # loop_counter goes down by 1
+            loop_counter -= 1
+
+        display.show(Image.HEART)
